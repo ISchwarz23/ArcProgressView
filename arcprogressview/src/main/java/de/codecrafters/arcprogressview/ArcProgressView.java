e18848722af984f9d57a307bf9b1b6c01d93f073
Ingo Schwarz <schwarz-ingo@web.de>
2015-10-11 13:00:59 +0200

    Add JavaDoc to ArcProgressView

--- arcprogressview/src/main/java/de/codecrafters/arcprogressview/ArcProgressView.java
+++ arcprogressview/src/main/java/de/codecrafters/arcprogressview/ArcProgressView.java
@@ -40,75 +40,180 @@ public class ArcProgressView extends View {
     private float progress;
 
 
+    /**
+     * Creates a new ArcProgressView with the given {@link Context}.
+     *
+     * @param context
+     *         The {@link Context} for which this ArcProgressView shall be created.
+     */
     public ArcProgressView(final Context context) {
         this(context, null);
     }
 
+    /**
+     * Creates a new ArcProgressView with the given {@link Context} and {@link AttributeSet}.
+     *
+     * @param context
+     *         The {@link Context} for which this ArcProgressView shall be created.
+     * @param attributes
+     *         The attributes that shall be used in this ArcProgressView.
+     */
     public ArcProgressView(final Context context, final AttributeSet attributes) {
         this(context, attributes, 0);
     }
 
+    /**
+     * Creates a new ArcProgressView with the given values.
+     *
+     * @param context
+     *         The {@link Context} for which this ArcProgressView shall be created.
+     * @param attributes
+     *         The attributes that shall be used in this ArcProgressView.
+     * @param defStyleAttr
+     *         The style attribute that shall be used int this ArcProgressView.
+     */
     public ArcProgressView(final Context context, final AttributeSet attributes, final int defStyleAttr) {
         super(context, attributes, defStyleAttr);
         initializeParams(context, attributes);
         initializePaint();
     }
 
-    private void initializePaint() {
-        paint.setAntiAlias(true);
-        paint.setTextAlign(Paint.Align.CENTER);
-    }
-
-    private void initializeParams(Context context, AttributeSet attributes) {
-        final TypedArray styledAttributes = context.obtainStyledAttributes(attributes, R.styleable.ArcProgressView);
-        range = styledAttributes.getInt(R.styleable.ArcProgressView_range, DEFAULT_RANGE);
-        rangePathColor = styledAttributes.getInt(R.styleable.ArcProgressView_rangePathColor, DEFAULT_RANGE_PATH_COLOR);
-        rangePathWidth = styledAttributes.getInt(R.styleable.ArcProgressView_rangePathWidth, DEFAULT_RANGE_PATH_WIDTH);
-        progress = styledAttributes.getFloat(R.styleable.ArcProgressView_progress, DEFAULT_PROGRESS);
-        progressPathColor = styledAttributes.getInt(R.styleable.ArcProgressView_progressPathColor, DEFAULT_PROGRESS_PATH_COLOR);
-        progressPathWidth = styledAttributes.getInt(R.styleable.ArcProgressView_progressPathWidth, DEFAULT_PROGRESS_PATH_WIDTH);
-        startAngle = styledAttributes.getInt(R.styleable.ArcProgressView_startAngle, DEFAULT_START_ANGLE);
-        styledAttributes.recycle();
-    }
-
+    /**
+     * Gives the current range of this {@link ArcProgressView}.
+     *
+     * @return The current range of this {@link ArcProgressView}.
+     */
     public int getRange() {
         return range;
     }
 
+    /**
+     * Sets the current range of this {@link ArcProgressView}.
+     *
+     * @param range
+     *         The current range of this {@link ArcProgressView}.
+     */
     public void setRange(int range) {
         this.range = range;
         invalidate();
     }
 
+    /**
+     * Gives the current start angle of this {@link ArcProgressView}.
+     *
+     * @return The current start angle of this {@link ArcProgressView}.
+     */
     public int getStartAngle() {
         return startAngle;
     }
 
+    /**
+     * Sets the current start angle of this {@link ArcProgressView}.
+     *
+     * @param startAngle
+     *         The current start angle of this {@link ArcProgressView}.
+     */
     public void setStartAngle(final int startAngle) {
         this.startAngle = startAngle;
         invalidate();
     }
 
+    /**
+     * Gives the current color of the range path.
+     *
+     * @return The current color of the range path.
+     */
+    public int getRangePathColor() {
+        return rangePathColor;
+    }
+
+    /**
+     * Sets the color of the range path.
+     *
+     * @param rangePathColor
+     *         The color that shall be set to the range path.
+     */
     public void setRangePathColor(final int rangePathColor) {
         this.rangePathColor = rangePathColor;
         invalidate();
     }
 
+
+    /**
+     * Sets the path width of the progress path.
+     *
+     * @return The path width of the progress path.
+     */
+    public int getProgressPathWidth() {
+        return progressPathWidth;
+    }
+
+    /**
+     * Sets the path width of the progress path.
+     *
+     * @param progressPathWidth
+     *         The path width that shall be set to the progress path.
+     */
     public void setProgressPathWidth(final int progressPathWidth) {
         this.progressPathWidth = progressPathWidth;
         invalidate();
     }
 
+    /**
+     * Gives the width of the range path.
+     *
+     * @return The width of the range path.
+     */
+    public int getRangePathWidth() {
+        return rangePathWidth;
+    }
+
+    /**
+     * Sets the width of the range path.
+     *
+     * @param rangePathWidth
+     *         The width that shall be set to the range path.
+     */
     public void setRangePathWidth(final int rangePathWidth) {
         this.rangePathWidth = rangePathWidth;
         invalidate();
     }
 
-    public void setProgressPathColor(int color) {
+    /**
+     * Gives the color of the progress path.
+     *
+     * @return The color of the progress path.
+     */
+    public int getProgressPathColor() {
+        return progressPathColor;
+    }
+
+    /**
+     * Sets the color of the progress path.
+     *
+     * @param color
+     *         The color that shall be set to the progress path
+     */
+    public void setProgressPathColor(final int color) {
         this.progressPathColor = color;
         invalidate();
     }
 
+    /**
+     * Gives the current progress.
+     *
+     * @return The current progress.
+     */
+    public float getProgress() {
+        return progress;
+    }
+
+    /**
+     * Sets the progress to the given value.
+     *
+     * @param progress
+     *         The progress that shall be set.
+     */
     public void setProgress(final float progress) {
         float progressToSet = progress;
 
@@ -125,12 +230,84 @@ public class ArcProgressView extends View {
         }
     }
 
-    public void setProgressAnimationEnabled(boolean animated) {
+    /**
+     * Indicates whether or not the progress animation is enabled.
+     *
+     * @return Whether or not the progress animation is enabled.
+     */
+    public boolean isProgressAnimationEnabled() {
+        return animateProgress;
+    }
+
+    /**
+     * Enables or disables the progress animation.
+     *
+     * @param animated
+     *         Whether or not the progress shall be animated.
+     */
+    public void setProgressAnimationEnabled(final boolean animated) {
         this.animateProgress = animated;
     }
 
-    public void setProgressAnimationDuration(long animationDuration) {
+    /**
+     * Gives the duration of a progress animations.
+     *
+     * @return The duration of progress animations.
+     */
+    public long getProgressAnimationDuration() {
+        return animationDuration;
+    }
+
+    /**
+     * Sets the duration of a progress animations.
+     *
+     * @param animationDuration
+     *         The duration that shall be used for progress animations.
+     */
+    public void setProgressAnimationDuration(final long animationDuration) {
         this.animationDuration = animationDuration;
+    }
+
+    /**
+     * Gives the {@link Interpolator} that is be used for progress animations.
+     *
+     * @return The {@link Interpolator} that is used for animating progress animations.
+     */
+    public Interpolator getProgressAnimationInterpolator() {
+        return interpolator;
+    }
+
+    /**
+     * Sets the {@link Interpolator} that shall be used for progress animations.
+     *
+     * @param interpolator
+     *         The {@link Interpolator} that shall be used for animating progress animations.
+     */
+    public void setProgressAnimationInterpolator(final Interpolator interpolator) {
+        this.interpolator = interpolator;
+    }
+
+
+    private void initializeParams(Context context, AttributeSet attributes) {
+        final TypedArray styledAttributes = context.obtainStyledAttributes(attributes, R.styleable.ArcProgressView);
+        range = styledAttributes.getInt(R.styleable.ArcProgressView_range, DEFAULT_RANGE);
+        rangePathColor = styledAttributes.getInt(R.styleable.ArcProgressView_rangePathColor, DEFAULT_RANGE_PATH_COLOR);
+        rangePathWidth = styledAttributes.getInt(R.styleable.ArcProgressView_rangePathWidth, DEFAULT_RANGE_PATH_WIDTH);
+        progress = styledAttributes.getFloat(R.styleable.ArcProgressView_progress, DEFAULT_PROGRESS);
+        progressPathColor = styledAttributes.getInt(R.styleable.ArcProgressView_progressPathColor, DEFAULT_PROGRESS_PATH_COLOR);
+        progressPathWidth = styledAttributes.getInt(R.styleable.ArcProgressView_progressPathWidth, DEFAULT_PROGRESS_PATH_WIDTH);
+        startAngle = styledAttributes.getInt(R.styleable.ArcProgressView_startAngle, DEFAULT_START_ANGLE);
+        styledAttributes.recycle();
+    }
+
+    private void initializePaint() {
+        paint.setAntiAlias(true);
+        paint.setTextAlign(Paint.Align.CENTER);
+    }
+
+    private void setProgressNotAnimated(final float progress) {
+        this.progress = progress;
+        invalidate();
     }
 
     private void setProgressAnimated(final float progress) {
@@ -155,15 +332,6 @@ public class ArcProgressView extends View {
             }
         };
         post(animator);
-    }
-
-    private void setProgressNotAnimated(final float progress) {
-        this.progress = progress;
-        invalidate();
-    }
-
-    public void setProgressAnimationInterpolator(Interpolator interpolator) {
-        this.interpolator = interpolator;
     }
 
     @Override
@@ -211,5 +379,4 @@ public class ArcProgressView extends View {
         cy = height / 2.0f + radius * (float) Math.sin(Math.toRadians(startAngle + range * progress));
         canvas.drawCircle(cx, cy, progressPathWidth / 2.0f, paint);
     }
-
 }
